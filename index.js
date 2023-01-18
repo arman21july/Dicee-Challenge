6632c7b1c202f6239e9e5513c8d2c0c5b38bbcf5
Md Arman <amd510025@gmail.com>
2023-01-18 15:36:11 +0000

    dice

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var img1 = "images/dice" + randomNumber1 + ".png";
+var img2 = "images/dice" + randomNumber2 + ".png";
+
+
+var a = document.querySelector(".dice .img1").setAttribute("src", img1);
+var b = document.querySelector(".dice .img2").setAttribute("src", img2);
+
+if (randomNumber1>randomNumber2) {
+     document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+
+}
+else if(randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!"
+}
